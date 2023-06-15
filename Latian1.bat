e0b0ff706159c867ebf5d949c670fe41039ad3ea
Yulfiatyas <136713019+Yulfiatyas@users.noreply.github.com>
2023-06-15 20:04:11 +0700

    Create Latian1.bat
    Menambahkan file latian 1.bat

---
+++ Latian1.bat
@@ -0,0 +1,12 @@
+ @echo off
+title IDENTITAS DIRI
+echo NIM                : 20122080
+echo Nama               : yulfia asriningtyas
+echo No. WA             : 087821515451
+echo Alamat             : Ds.kandeman
+echo Status             : Belum Menikah
+echo Motto              : Bersusah-sudah dahulu bersenang senang kemudian
+echo Pacar              : i
+echo Rencana Menikah    : 1 Istri Udah Cukup
+echo Rencana Anak       : 3 Boleh
+pause
